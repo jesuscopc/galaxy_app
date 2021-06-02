ff177f34fe171a11bce07a3f5f6ab9e065744148
Jesus <jesuscopc@gmail.com>
2021-06-01 21:58:39 -0500

    chore: Added proyect from scratch with react webpack and typescript

---
+++ config/webpack.common.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    bundle: path.resolve(__dirname, '..', './src/index.tsx'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+      {
+        test: /\.(woff|woff2|ttf)$/,
+        use: {
+          loader: 'url-loader?limit=1024&name=fonts/[name].[ext]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      favicon: path.resolve(__dirname, '../', './public/favicon.png'),
+      title: 'Galaxy App Setup Advanced',
+      template: path.resolve(__dirname, '..', './public/index.html'),
+    }),
+  ],
+  output: {
+    path: path.join(__dirname, '..', 'dist'),
+    filename: '[name].[chunkhash].bundle.js',
+    chunkFilename: '[name].[chunkhash].bundle.js',
+    publicPath: '/',
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.scss';
+
+const App = (): React.ReactElement => {
+  return (
+    <h1>My galaxy App</h1>
+  );
+};
+
+export default App;
