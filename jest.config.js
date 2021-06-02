b2d3b45a634379b75fe4ef6561757f83dd3042b3
Jesus <jesuscopc@gmail.com>
2021-06-01 23:16:15 -0500

    chore: Add jest config enviroment to project

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "preset": "ts-jest",
+    "collectCoverage": true,
+    "roots": [
+      "<rootDir>/src"
+    ],
+    "transform": {
+      "^.+\\.(ts|tsx)$": "ts-jest",
+      "^.+\\.(js|jsx)$": "babel-jest"
+    },
+    "transformIgnorePatterns": [
+      "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"
+    ],
+    "moduleFileExtensions": [
+      "ts",
+      "tsx",
+      "js",
+      "jsx",
+      "json",
+      "node"
+    ],
+    "setupFiles": ['<rootDir>/src/setupTests.js'],
+    "moduleNameMapper":{
+      "\\.(scss|css)$": "<rootDir>/styleMock.js"
+    },
+    "collectCoverageFrom": [
+      "src/**/*.{ts,tsx}",
+    ],
+    "coveragePathIgnorePatterns": [
+      "src/index.tsx",
+    ]
+  }
